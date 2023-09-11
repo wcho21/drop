379d0c43ed0f4bc0bcf34e4a94822ee3452a0e9b
W. Cho <wcho.dev@gmail.com>
2023-09-11 13:51:07 +0900

    use google fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Noto Sans KR"', ...defaultTheme.fontFamily.sans],
+        display: ['"Secular One"', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
 }
