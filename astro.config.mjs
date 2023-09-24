75226f80d27d731c45077ecb85a56247bf9e2177
W. Cho <wcho.dev@gmail.com>
2023-09-25 07:02:44 +0900

    better code display

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,10 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx()],
+  markdown: {
+    shikiConfig: {
+      theme: "dracula",
+    },
+  },
 });
