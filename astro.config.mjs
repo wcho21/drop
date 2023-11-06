8727c1f0e786e5c5202d7e25ff0a0f12b25cd149
W. Cho <wcho.dev@gmail.com>
2023-11-06 22:35:48 +0900

    remove blank line

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
