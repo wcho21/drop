0a9e9a3b92891fd8eb34588631d2558571321b76
W. Cho <wcho.dev@gmail.com>
2024-02-01 18:38:09 +0900

    add series fields

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,14 @@
 import { z, defineCollection } from "astro:content";
 
+const defaultSeriesName = "Single articles";
+
 const blogCollection = defineCollection({
   type: "content",
   schema: ({ image }) => z.object({
     title: z.string(),
     date: z.date(),
     summary: z.string(),
+    series: z.string().optional().transform(s => s ?? defaultSeriesName),
     thumbnail: image(),
   }),
 });
