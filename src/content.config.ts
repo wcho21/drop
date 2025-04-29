ca103fa01f595f7ccd71b4c530fd1417785a946c
Cho <wcho.dev@gmail.com>
2025-04-29 16:06:25 +0000

    replace legacy api

---
+++ src/content.config.ts
@@ -0,0 +1,16 @@
+import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
+
+const blog = defineCollection({
+  loader: glob({ pattern: "**/[^_]*.mdx", base: "./src/content/blog" }),
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    summary: z.string(),
+    thumbnail: z.string(),
+    series: z.string().optional(),
+    featured: z.boolean().optional(),
+  }),
+});
+
+export const collections = { blog };
