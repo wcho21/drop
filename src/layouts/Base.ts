a7588d391a3f504eca079bf6097a1b584e5f30d8
Cho <wcho.dev@gmail.com>
2025-04-29 11:59:36 +0000

    improve names and specify return type

--- src/layouts/Base.ts
+++ src/layouts/Base.ts
@@ -1,9 +1,9 @@
-export const makeTitle = (
+export function generateTitle(
   pageTitle: string,
   siteTitle: string,
   separator: string,
-) => {
+): string {
   const title = [pageTitle, siteTitle].join(separator);
 
   return title;
-};
+}
