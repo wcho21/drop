94ea18aae511c44ee2cff623614f9509d4176c93
Cho <wcho.dev@gmail.com>
2025-04-29 17:58:09 +0000

    specify return type

--- src/scripts/utils/collections/index.ts
+++ src/scripts/utils/collections/index.ts
@@ -1,7 +1,9 @@
 import type { CollectionEntry } from "astro:content";
 import { createDateComparator } from "../comparators";
 
-export function sortBlogCollectionByDate(entries: CollectionEntry<"blog">[]) {
+export function sortBlogCollectionByDate(
+  entries: CollectionEntry<"blog">[],
+): CollectionEntry<"blog">[] {
   const decOrderDateComparator = createDateComparator("decreasing");
   const sorted = entries.toSorted((entry1, entry2) =>
     decOrderDateComparator(entry1.data.date, entry2.data.date),
@@ -10,14 +12,20 @@ export function sortBlogCollectionByDate(entries: CollectionEntry<"blog">[]) {
   return sorted;
 }
 
-export function selectFeatured(entries: CollectionEntry<"blog">[]) {
+export function selectFeatured(
+  entries: CollectionEntry<"blog">[],
+): CollectionEntry<"blog">[] {
   return entries.filter(entry => (entry.data.featured ?? false) === true);
 }
 
-export function selectNonfeatured(entries: CollectionEntry<"blog">[]) {
+export function selectNonfeatured(
+  entries: CollectionEntry<"blog">[],
+): CollectionEntry<"blog">[] {
   return entries.filter(entry => (entry.data.featured ?? false) === false);
 }
 
-export function selectSeries(entries: CollectionEntry<"blog">[]) {
+export function selectSeries(
+  entries: CollectionEntry<"blog">[],
+): CollectionEntry<"blog">[] {
   return entries.filter(entry => entry.data.series !== undefined);
 }
