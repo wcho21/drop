e331e72e62802fe079cfcfaa5ef83e75aaee7433
Cho <wcho.dev@gmail.com>
2025-04-29 18:59:38 +0000

    factor out profile constants

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,5 +4,9 @@ const config = {
     titleSep: " | ",
     subtitle: "A magazine about computation.",
   },
+  contacts: {
+    github: "https://github.com/wcho21",
+    email: "mailto:contact@rooi.dev",
+  },
 } as const;
 export default config;
