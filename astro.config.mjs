a10ba8a3b13d6141629c5086b8ab670b765822ad
Cho <wcho.dev@gmail.com>
2025-05-07 01:01:55 +0900

    format codes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import tailwindcss from "@tailwindcss/vite";
 import {
   transformerMetaWordHighlight,
   transformerNotationDiff,
   transformerNotationErrorLevel,
   transformerNotationHighlight,
 } from "@shikijs/transformers";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
