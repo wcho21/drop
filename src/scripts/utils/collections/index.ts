8f9d53f9f9e964e7707c48bd97fb68128f9ddbe2
Cho <wcho.dev@gmail.com>
2025-05-22 10:00:00 +0000

    format codes

--- src/scripts/utils/collections/index.ts
+++ src/scripts/utils/collections/index.ts
@@ -10,32 +10,22 @@ interface Series {
 export type SeriesMap = Map<string, CollectionEntry<"blog">[]>;
 
 /** Returns entries sorted in decreasing order by date */
-export function sortBlogCollectionByDate(
-  entries: CollectionEntry<"blog">[],
-): CollectionEntry<"blog">[] {
+export function sortBlogCollectionByDate(entries: CollectionEntry<"blog">[]): CollectionEntry<"blog">[] {
   const decOrderDateComparator = createDateComparator("decreasing");
-  const sorted = entries.toSorted((entry1, entry2) =>
-    decOrderDateComparator(entry1.data.date, entry2.data.date),
-  );
+  const sorted = entries.toSorted((entry1, entry2) => decOrderDateComparator(entry1.data.date, entry2.data.date));
 
   return sorted;
 }
 
-export function selectFeatured(
-  entries: CollectionEntry<"blog">[],
-): CollectionEntry<"blog">[] {
+export function selectFeatured(entries: CollectionEntry<"blog">[]): CollectionEntry<"blog">[] {
   return entries.filter(entry => (entry.data.featured ?? false) === true);
 }
 
-export function selectNonfeatured(
-  entries: CollectionEntry<"blog">[],
-): CollectionEntry<"blog">[] {
+export function selectNonfeatured(entries: CollectionEntry<"blog">[]): CollectionEntry<"blog">[] {
   return entries.filter(entry => (entry.data.featured ?? false) === false);
 }
 
-export function selectSeries(
-  entries: CollectionEntry<"blog">[],
-): CollectionEntry<"blog">[] {
+export function selectSeries(entries: CollectionEntry<"blog">[]): CollectionEntry<"blog">[] {
   return entries.filter(entry => entry.data.series !== undefined);
 }
 
